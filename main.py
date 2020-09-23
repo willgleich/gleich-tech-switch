464c869175015c4630560060acc7676ec889ff10
Will <22464726+willgleich@users.noreply.github.com>
2020-09-22 21:06:29 -0600

    Classify and add unauthenticated

--- main.py
+++ main.py
@@ -7,33 +7,52 @@ from google.cloud import secretmanager
 logging.basicConfig(level=logging.INFO)
 logging.getLogger('googleapicliet.discovery_cache').setLevel(logging.ERROR)
 
-def create_cloud_run_service(cloud_run, svc_name):
-    body = {'apiVersion': 'serving.knative.dev/v1',
-            'kind': 'Service',
-            'metadata': {'name': svc_name,
-                         'namespace': '248394897420',
-                         'generation': 1},
-            'spec': {'template': {'metadata': {'name': 'gleich-tech-00001-qel',
-                                               'annotations': {'run.googleapis.com/client-name': 'cloud-console',
-                                                               'autoscaling.knative.dev/maxScale': '1000'}},
-                                  'spec': {'containerConcurrency': 80,
-                                           'timeoutSeconds': 300,
-                                           'containers': [{'image': 'gcr.io/main-285019/resume',
-                                                           'resources': {'limits': {'cpu': '1000m', 'memory': '256Mi'}},
-                                                           'ports': [{'containerPort': 8080}]}]}},
-                     'traffic': [{'percent': 100, 'latestRevision': True}]}}
-    return cloud_run.projects().locations().services().create(parent="projects/248394897420/locations/us-west1", body=body).execute()
 
-def delete_cloud_run_service(cloud_run, svc_name):
-    return cloud_run.projects().locations().services().delete(name="projects/248394897420/locations/us-west1/services/" + svc_name).execute()
+class cloud_run_service(object):
+    def __init__(self, service_name, project, location):
+        self.service_name = service_name
+        self.project = project
+        self.location = location
+        self.cloud_run = googleapiclient.discovery.build('run', 'v1')
 
-def service_exists(cloud_run, svc_name):
-    r = cloud_run.projects().locations().services().list(parent="projects/248394897420/locations/us-west1").execute()
-    if "items" in r.keys():
-        for run_svc in r["items"]:
-            if run_svc["metadata"]["name"] == svc_name:
-                return True
-    return False
+    def create(self, image):
+        body = {'apiVersion': 'serving.knative.dev/v1',
+                'kind': 'Service',
+                'metadata': {'name': self.service_name,
+                             'namespace': '248394897420',
+                             'generation': 1},
+                'spec': {'template': {'metadata': {'name': 'gleich-tech-00001-qel',
+                                                   'annotations': {'run.googleapis.com/client-name': 'cloud-console',
+                                                                   'autoscaling.knative.dev/maxScale': '1000'}},
+                                      'spec': {'containerConcurrency': 80,
+                                               'timeoutSeconds': 300,
+                                               'containers': [{'image': image,
+                                                               'resources': {
+                                                                   'limits': {'cpu': '1000m', 'memory': '256Mi'}},
+                                                               'ports': [{'containerPort': 8080}]}]}},
+                         'traffic': [{'percent': 100, 'latestRevision': True}]}}
+        return self.cloud_run.projects().locations().services().create(
+            parent=f"projects/{self.project}/locations/us-west1", body=body).execute()
+
+    def delete(self):
+        return self.cloud_run.projects().locations().services().delete(
+            name=f"projects/{self.project}/locations/us-west1/services/" + self.service_name).execute()
+
+    def exists(self):
+        r = self.cloud_run.projects().locations().services().list(
+            parent=f"projects/{self.project}/locations/us-west1").execute()
+        if "items" in r.keys():
+            for run_svc in r["items"]:
+                if run_svc["metadata"]["name"] == self.service_name:
+                    return True
+        return False
+
+    def allow_unauthenticated(self):
+        policy = {'policy': {'bindings': [{'role': 'roles/run.invoker', 'members': ['allUsers']}]}}
+        self.cloud_run.projects().locations().services().setIamPolicy(
+            resource=f"projects/{self.project}/locations/{self.location}/services/{self.service_name}",
+            body=policy).execute()
+
 
 def get_secret(secret_name):
     client = secretmanager.SecretManagerServiceClient()
@@ -43,18 +62,20 @@ def get_secret(secret_name):
     response = client.access_secret_version(resource_name)
     return response.payload.data.decode('UTF-8')
 
+
 def gleich_switch(request):
+    gleich_tech = cloud_run_service("gleich-tech", "248394897420", "us-west1")
     logging.info("started the function")
-    cloud_run = googleapiclient.discovery.build('run', 'v1')
     logging.info("initalized the cloud_run")
     secret_name = get_secret("cloudflare-api-key")
-    if not service_exists(cloud_run, "gleich-tech"):
-        svc = create_cloud_run_service(cloud_run, "gleich-tech")
+    if not gleich_tech.exists():
+        svc = gleich_tech.create("gcr.io/main-285019/resume")
         logging.info("created gleich-tech svc")
-        # print(svc)
+        gleich_tech.allow_unauthenticated()
     else:
         logging.info("svc gleich-tech already exists")
     return f"function moved through successfully"
 
+
 if __name__ == '__main__':
-    handler({})
+    gleich_switch({})
